674fbec3921e2fef28617940f46f35e0568378c1
sonickmumba <kingellie.mumba@gmail.com>
2023-01-17 22:35:28 +0200

    Implement the recipe list and recipe details pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,42 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def public
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @foods = Food.all
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    if @recipe.destroy
+      redirect_to recipes_path, notice: 'Recipe successfully deleted'
+    else
+      render :new
+    end
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe added successfully'
+    else
+      render :new, alert: 'Recipe not successfully added'
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_011920) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_070306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_17_011920) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "prepatation_time"
+    t.string "preparation_time"
     t.string "cooking_time"
     t.text "description"
     t.boolean "public"
