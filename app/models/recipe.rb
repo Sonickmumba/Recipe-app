88c9cea79768aa31dcdedac83e388b50873eeeca
sonickmumba <kingellie.mumba@gmail.com>
2023-01-17 02:35:50 +0200

    Fix linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,5 +7,5 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
-  validates :description, presence: true, length: { minimum: 4}
+  validates :description, presence: true, length: { minimum: 4 }
 end
